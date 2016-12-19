dc890c7b04d9a73114f5d11a9b7f7efed0d05fdc
Aravind Sankarappan <aravind@Aravinds-MacBook-Air.local>
2016-12-19 22:54:24 +0530

    Initial Commit

---
+++ Sample/AppDelegate.m
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.m
+//  Sample
+//
+//  Created by Aravind Sankarappan on 19/12/16.
+//  Copyright © 2016 Aravind Sankarappan. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+
+@end
---
+++ Sample/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Sample
+//
+//  Created by Aravind Sankarappan on 19/12/16.
+//  Copyright © 2016 Aravind Sankarappan. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
